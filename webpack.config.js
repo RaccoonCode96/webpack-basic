d5471019447569ca1d45ec2baedc6f44bc36d691
RaccoonCode96 <waawoo13@gmail.com>
2021-06-03 17:11:08 +0900

    upload files

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+	entry: './js/main.js',
+	output: {
+		// path: path.resolve(__dirname, 'dist'),
+		// filename: 'main.js',
+		clean: true,
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.s?css$/,
+				use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+			},
+			{
+				test: /\.js$/,
+				use: ['babel-loader'],
+			},
+		],
+	},
+	// 번들링 후 결과물의 처리 방식 등 다양한 플러그인들을 설정
+	plugins: [
+		new HtmlPlugin({
+			template: './index.html',
+		}),
+		new CopyPlugin({
+			patterns: [{ from: 'static' }],
+		}),
+	],
+
+	devServer: {
+		host: 'localhost',
+	},
+};
